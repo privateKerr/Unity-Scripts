496336ca030ccc25395e30e9cd6d63df46e7ba9e
Hayden Kerr <10897906@my.uvu.edu>
2024-02-01 19:56:52 -0700

    Added DraggableBehaviour script for color matching game

--- Color Matching/DraggableBehaviour.cs
+++ Color Matching/DraggableBehaviour.cs
@@ -1,13 +1,39 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DraggableBehaviour : MonoBehaviour
 {
     private Camera cameraObj;
+    public bool draggable;
+    public Vector3 position, offset;
+    public UnityEvent startDragEvent, endDragEvent;
     
     private void Start()
     {
+        cameraObj = Camera.main;
+    }
+
+    public IEnumerator OnMouseDown()
+    {
+        offset = transform.position - cameraObj.ScreenToWorldPoint(Input.mousePosition);
+        startDragEvent.Invoke();
+        draggable = true;
+        yield return new WaitForFixedUpdate();
+
+        while (draggable)
+        {
+            yield return new WaitForFixedUpdate();
+            position = cameraObj.ScreenToWorldPoint(Input.mousePosition) + offset;
+            transform.position = position;
+        }
         
+    }
+    
+    private void OnMouseUp()
+    {
+        draggable = false;
+        endDragEvent.Invoke();
     }
 }
