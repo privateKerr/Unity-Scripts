2d236c68ee345f0e9c0b48449020aaffe2b18d9d
Hayden Kerr <10897906@my.uvu.edu>
2024-02-21 17:18:28 -0700

    script updates

--- CoroutineBehaviour.cs
+++ CoroutineBehaviour.cs
@@ -13,6 +13,12 @@ public class CoroutineBehaviour : MonoBehaviour
     private WaitForSeconds wfsObj;
     private WaitForFixedUpdate wffuObj;
 
+    public bool CanRun
+    {
+        get => canRun;
+        set => canRun = value;
+    }
+
     private void Start()
     {
         wfsObj = new WaitForSeconds(seconds);
